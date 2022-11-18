05fbb18d1a625c927f6b0a905c7af5e673c08113
Marco Zamprogno <marco.zamprogno@it.clara.net>
2022-11-18 15:42:20 +0100

    add accumulator logic

--- src/main/kotlin/it/giannibombelli/refactor_challenge/FizzBuzz.kt
+++ src/main/kotlin/it/giannibombelli/refactor_challenge/FizzBuzz.kt
@@ -2,16 +2,22 @@ package it.giannibombelli.refactor_challenge
 
 class FizzBuzz : FizzBuzzInterface {
     override fun say(number: Int): String {
+        var accumulator = ""
         if (number % 3 == 0 && number % 5 == 0) {
             return "Fizz" + "Buzz"
         }
         if (number % 3 == 0) {
+            accumulator += "Fizz"
             return "Fizz"
         }
         if (number % 5 == 0) {
+            accumulator += "Buzz"
             return "Buzz"
         }
 
-        return number.toString()
+        if (accumulator.isEmpty())
+            return number.toString()
+        else
+            return accumulator
     }
 }
