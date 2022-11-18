70675b9dbe3aedb02fe27d6798f6e97eb5259277
Marco Zamprogno <marco.zamprogno@it.clara.net>
2022-11-18 15:43:18 +0100

    remove useless if branch

--- src/main/kotlin/it/giannibombelli/refactor_challenge/FizzBuzz.kt
+++ src/main/kotlin/it/giannibombelli/refactor_challenge/FizzBuzz.kt
@@ -3,21 +3,17 @@ package it.giannibombelli.refactor_challenge
 class FizzBuzz : FizzBuzzInterface {
     override fun say(number: Int): String {
         var accumulator = ""
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "Fizz" + "Buzz"
-        }
+
         if (number % 3 == 0) {
             accumulator += "Fizz"
-            return "Fizz"
         }
         if (number % 5 == 0) {
             accumulator += "Buzz"
-            return "Buzz"
         }
 
-        if (accumulator.isEmpty())
-            return number.toString()
+        return if (accumulator.isEmpty())
+            number.toString()
         else
-            return accumulator
+            accumulator
     }
 }
